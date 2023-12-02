fa81fded6c5142137ae607e31dedb4f1dcfd665b
Anh Tran <anhtr077@gmail.com>
2023-12-02 08:57:11 -0800

    Add files to Github

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,10 @@
 for (let i = 1; i <= 50; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("frizz"); 
+        console.log("frizzbuzz"); 
     } else if (i % 5 === 0) {
         console.log("buzz");
     } else if (i % 3 === 0) {
-        console.log("fizzbuzz");
+        console.log("fizz");
     } else {
         console.log(i)
     }   
---
+++ maxoftwo.js
@@ -0,0 +1,12 @@
+x = 2;
+y = 2;
+
+// if the values are identical
+if (x === y){
+    console.log("The values are identical.");
+} else {
+    console.log(Math.max(x, y));//print the largest value
+}
+    
+
+
